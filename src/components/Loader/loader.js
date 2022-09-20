f10ba3b4938cbc4fe85a87e6c785c7eed1e1986c
Alieltahan <alyaltahan@gmail.com>
2022-09-20 15:13:54 +0200

    Adding Modal to Loader

--- src/components/Loader/loader.js
+++ src/components/Loader/loader.js
@@ -1,10 +1,13 @@
+import { Modal } from '../ProductCard/ProductCardStyles';
 import './loader.styles.css';
 
 const Loader = () => {
   return (
-    <div className="container">
-      <div className="loader"></div>
-    </div>
+    <Modal open>
+      <div className="container">
+        <div className="loader"></div>
+      </div>
+    </Modal>
   );
 };
 
