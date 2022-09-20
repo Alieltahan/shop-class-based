6ac06e8f43c4ea150ca3e9620e47190883cb6a9d
Alieltahan <alyaltahan@gmail.com>
2022-09-20 13:15:55 +0200

    init class based

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/layout/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Toaster } from 'react-hot-toast';
+import { createGlobalStyle } from 'styled-components';
+import Loader from './components/Loader/loader';
+
+const GlobalStyles = createGlobalStyle`
+:root{
+  --c-text: #1D1F22;
+  --c-white: #FFFFFF;
+  --c-primary: #5ECE7B;
+  --c-black: #1D1F22;
+  --c-info-variant: #B4D2F4;
+  --price-regular-font: Raleway;
+  --product-card-box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+  /* given Shadow box effect  */
+  /* --product-card-box-shadow: 0px 4px 35px rgba(168, 172, 176, 0.19); */
+  --button-box-shadow: drop-shadow(0px 4px 11px rgba(29, 31, 34, 0.1));
+}
+ *,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  //defines the rem! 1 rem = 10 px
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+  transition: all 0.3s;
+}
+
+body {
+  transition: inherit;
+  scroll-behavior: inherit;
+  box-sizing: border-box;
+}
+
+::selection {
+  background-color: #04066b;
+  color: white;
+}
+
+a,
+a:link:active:hover {
+  text-decoration: none;
+}
+a:visited, a {
+  color: var(--c-text);
+  > div{
+    color: inherit;
+  }
+}
+
+img {
+  width: 100%;
+}
+
+// Typography
+body {
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 1.5;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  line-height: 1.1;
+}
+h1 {
+  font-size: 6rem;
+}
+h2 {
+  font-size: 5rem;
+}
+h3 {
+  font-size: 4rem;
+}
+h4 {
+  font-size: 3rem;
+}
+
+.active{
+    color: var(--c-primary);
+    border-bottom: 2px solid var(--c-primary);
+    > div{
+      color: var(--c-primary);
+    }
+  }
+  .EUR:before {
+    content: '\u20AC';
+  }
+  .USD:before {
+    content: '\u0024';
+  }
+  .GBP:before {
+    content: '£';
+  }
+  .RUB:before {
+    content: '₽';
+  }
+  .JPY:before {
+    content: '¥';
+  }
+  /* For Scability, Just add className with Currency Symbol & Add It To The CONST Symbol */
+  .AUD:before {
+    content: 'A$';
+  }
+  .selected{
+    background-color: #000;
+    color: white;
+    &-mini{
+      background-color: #fff;
+      opacity: 0.5;
+      color: #000;
+    }
+  }
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <GlobalStyles />
+        <Main />
+        <Toaster />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { InMemoryCache } from '@apollo/client';
+import confStore from './components/store/confStore';
+import { Provider } from 'react-redux';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { Toaster } from 'react-hot-toast';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000',
+});
+// client
+//   .query({
+//     query: gql`
+//       query QUERY_CATEGORIES {
+//         categories {
+//           name
+//         }
+//       }
+//     `,
+//   })
+//   .then((da) => console.log(da));
+
+const store = confStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <App />
+      <Toaster />
+    </ApolloProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
