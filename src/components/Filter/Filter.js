57eccab05013c91b38925d545a927ca263d0b94b
Alieltahan <alyaltahan@gmail.com>
2022-09-25 21:03:16 +0200

    Filter Component & styling

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,145 @@
+import { Component } from 'react';
+import { compose } from 'redux';
+import CheckBox from '../common/Checkbox/Checkbox';
+import { getUniqueArray } from '../lib/ArrayUniqueValues';
+import { getFilteredProducts } from '../lib/FilterFn';
+import { withRouter } from '../lib/WithHOC/ReactRouter/WithRouter';
+import { WithForm } from '../lib/WithHOC/withForm/WithForm';
+import './Filter.scss';
+
+class Filter extends Component {
+  state = {
+    productsAttributes: {},
+  };
+  componentDidMount() {
+    this.getAllAttributes();
+  }
+  // function to get All Attributes from all the products and set the state.
+  getAllAttributes() {
+    const Attributes = {};
+    this.props.products.map((p) =>
+      p.attributes.map((at) => {
+        let property = at.name.split(' ').join('_');
+        if (!Attributes[property]) {
+          Attributes[property] = [];
+        }
+        Attributes[property].push(...at.items.map((v) => v.value));
+        // to remove duplicated values in the Array.
+        Attributes[property] = getUniqueArray(Attributes[property]);
+      })
+    );
+
+    this.setState({ productsAttributes: { ...Attributes } });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { inputs, onSubmit } = this.props;
+    onSubmit(inputs);
+  };
+
+  handleResetFilter = () => {
+    this.props.resetForm();
+    this.props.onSearchParams({});
+    this.props.onReset();
+  };
+  /**
+   * @param {Object.key} k Object Key of attributes.
+   * @returns select Html element
+   */
+  renderSelectElements = (k) => (
+    <select
+      className="attribute-select"
+      onChange={this.props.handleChange}
+      name={k}
+      id={k}
+    >
+      <option value=""></option>
+      {this.state.productsAttributes[k].map((v) => {
+        return (
+          <option name={k} key={v} value={v}>
+            {v}
+          </option>
+        );
+      })}
+    </select>
+  );
+
+  renderColorBoxes = (k) => {
+    return this.state.productsAttributes[k].map((v) => {
+      return (
+        <span
+          name={k}
+          value={v}
+          onClick={() => this.props.handleFilterColors(v)}
+          key={v}
+          className={
+            this.props.inputs[k] === v
+              ? 'color__box attribute-selectedColor '
+              : 'color__box'
+          }
+          style={{ backgroundColor: `${v}` }}
+        />
+      );
+    });
+  };
+
+  renderCheckBox = (k) => {
+    return (
+      <CheckBox
+        onChange={this.props.handleChange}
+        onInput={this.props.inputs}
+        name={k}
+      />
+    );
+  };
+
+  render() {
+    // console.log(this.state.productsAttributes);
+    const { handleChange, inputs, resetForm, products } = this.props;
+    // console.log(products);
+    const { productsAttributes } = this.state;
+    // console.log(`ATTRIBUTES`, this.state.);
+
+    return (
+      <div className="filter__container">
+        <form className="form" onSubmit={this.handleSubmit} method="POST">
+          <legend>
+            <strong>Filter Products</strong>
+          </legend>
+          {Object.keys(productsAttributes).map((k) => {
+            return (
+              <div className="attribute-key" key={k}>
+                <label>{k.split('_').join(' ')} : </label>
+                {k === 'Color'
+                  ? this.renderColorBoxes(k)
+                  : productsAttributes[k][0].toLowerCase() === 'yes' ||
+                    productsAttributes[k][0].toLowerCase() === 'no'
+                  ? this.renderCheckBox(k)
+                  : this.renderSelectElements(k)}
+              </div>
+            );
+          })}
+          <div className="form__btn-container">
+            <button
+              className="form__btn"
+              type="submit"
+              value="submit"
+              disabled={Object.keys(inputs).length === 0}
+            >
+              Filter
+            </button>
+            <button
+              className="form__btn form__btn-reset"
+              type="reset"
+              onClick={this.handleResetFilter}
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default compose(withRouter, WithForm)(Filter);
--- src/components/Products/Products.styles.js
+++ src/components/Products/Products.styles.js
@@ -17,8 +17,11 @@ export const ProductsContainer = styled.div`
   grid-template-columns: repeat(auto-fit, 38.6rem);
   column-gap: 4rem;
   row-gap: 10.3rem;
+  position: relative;
+  padding-left: 25rem;
+
   @media only screen and (max-width: 65.625em) {
-    grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+    grid-template-columns: 30rem repeat(auto-fit, minmax(30rem, 1fr));
     justify-items: center;
   }
 `;
